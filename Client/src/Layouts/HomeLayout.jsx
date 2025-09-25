fcb9a97c94346eb7f7fbac7310846402aa652290
kmanthugowda <kmanthugowda@gmail.com>
2025-09-25 09:34:07 +0000

    updated components spelling

--- Client/src/Layouts/HomeLayout.jsx
+++ Client/src/Layouts/HomeLayout.jsx
@@ -3,7 +3,7 @@ import { FiMenu } from 'react-icons/fi';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 
-import Footer from '../Compontents/Footer.jsx';
+import Footer from '../Components/Footer.js';
 import { logout } from '../Redux/Slices/AuthSlice.js';
 import logo from '../Assets/Images/logoimage.png';
 
