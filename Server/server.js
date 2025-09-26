3de8d2186a26da522b9604382d3b64e313326327
Manthu-Gowda <147706528+Manthu-Gowda@users.noreply.github.com>
2025-09-26 12:17:06 +0530

    removed dist folder from gitignore

--- Server/server.js
+++ Server/server.js
@@ -3,19 +3,19 @@ import cloudinary from 'cloudinary';
 import connectionToDB from './config/dbConnection.js';
 import app from './app.js';
 
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 /**
  * @Cloudinary configuration for file storage service
  */
 cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET,
-    secure:process.env.CLOUDINARY_SECURE,
-})
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+    secure: process.env.CLOUDINARY_SECURE,
+});
 
-app.listen(PORT,async ()=>{
+app.listen(PORT, async () => {
     await connectionToDB();
-    console.log(`App is running at  http:localhost:${PORT} `);
-})
+    console.log(`App is running at http://localhost:${PORT} or on Render at ${process.env.FRONTEND_URL}`);
+});
